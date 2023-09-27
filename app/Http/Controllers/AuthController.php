250b37aaaae2d8d6d7bbaa97212e44fef5cc046f
Sarah JOYEUSE <joysarah15@gmail.com>
2023-09-27 14:45:56 +0000

    methode supprimé fait par SARAH

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,17 +12,6 @@ class AuthController extends Controller
         return view('Auth');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function login(Request $request)
-    {
-        if(Auth::attempt(['username'=>$request->username, 'password'=>$request->password])){
-            return view('dashboard');
-        }
-
-        return view('Auth.login')->with('status', 'Identifiant ou mot de passe incorrect.');
-
     public function login(Request $request){
 
        if(Auth::attempt(['username'=>$request->username, 'password'=>$request->password])){
@@ -31,8 +20,6 @@ class AuthController extends Controller
        return view('Auth')->with('status', 'identifiant ou mot de passe incorrect');
     }
 
-    public function logout()
-    {
     public function logout(){
         Auth::logout();
         return view('Auth');
