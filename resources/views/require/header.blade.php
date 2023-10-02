d0c6df72cc7e37ee8d7e44838a3542f2a7989b9a
Sarah JOYEUSE <joysarah15@gmail.com>
2023-10-02 16:12:43 +0000

    recevoir la correction

--- resources/views/require/header.blade.php
+++ resources/views/require/header.blade.php
@@ -175,15 +175,15 @@
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
               <h6>{{Auth::user()->name}}</h6>
-              <span>Web Designer</span>
+              <span>administateur</span>
             </li>
             <li>
               <hr class="dropdown-divider">
             </li>
 
-            
 
-          
+
+
 
             <li>
               <a class="dropdown-item d-flex align-items-center" href="{{ route('logout') }}">
